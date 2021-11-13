3f7adde6c72540a4e5d9e18aab2abbc0ba499a0b
manjudalai <manju.ivision@gmail.com>
2021-11-13 13:21:17 +0530

    login Authentication

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const productRoutes = require("./routes/products");
 const producttasksRoutes = require("./routes/products-tasks");
+const authroutes = require('./routes/authroutes');
 const mysqlConnection = require("./connection")
  
 var app = express();
@@ -11,6 +12,18 @@ app.use(bodyParser.json());
 
 app.use(productRoutes);
 app.use(producttasksRoutes);
+app.use(authroutes);
+
+// Handling Errors
+app.use((err, req, res, next) => {
+    // console.log(err);
+    err.statusCode = err.statusCode || 500;
+    err.message = err.message || "Internal Server Error";
+    res.status(err.statusCode).json({
+      message: err.message,
+    });
+});
+
 
 app.listen(3000, () => {
     console.log('Server is running at port 3000');
